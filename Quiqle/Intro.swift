77eba80c73d9ef3301ce1e0887e4e84899ab4be1
Sikander Zeb <sikanderzb@gmail.com>
2018-05-30 00:29:53 +0400

    initial ui

---
+++ Quiqle/Intro.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Quiqle
+//
+//  Created by Sikander Zeb on 5/29/18.
+//  Copyright © 2018 Sikander Zeb. All rights reserved.
+//
+
+import UIKit
+
+class Intro: UIViewController {
+
+    @IBOutlet weak var collection: UICollectionView!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    let imagesArray = [#imageLiteral(resourceName: "slide1"), #imageLiteral(resourceName: "slide2"), #imageLiteral(resourceName: "slide3")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0.0
+        layout.minimumInteritemSpacing = 0.0
+        layout.itemSize = collection.bounds.size
+        collection.setCollectionViewLayout(layout, animated: false)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension Intro : UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let image = cell.viewWithTag(1) as! UIImageView
+        image.image = imagesArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        //let currentIndex = (collection.contentOffset.x / collection.frame.size.width + 0.5) as! Int;
+        pageControl.currentPage = collection.indexPathsForVisibleItems[0].row;
+        if pageControl.currentPage == 0 {
+            welcomeLabel.text = "Welcome"
+        }
+        else if pageControl.currentPage == 1 {
+            welcomeLabel.text = "Connect"
+        }
+        else  {
+            welcomeLabel.text = "Hang Out"
+        }
+    }
+}
+
+
+
---
+++ Quiqle/Utilities/Extensions.swift
@@ -0,0 +1,56 @@
+//
+//  Extensions.swift
+//  Quiqle
+//
+//  Created by Sikander Zeb on 5/29/18.
+//  Copyright © 2018 Sikander Zeb. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
+extension UIViewController {
+    
+    @IBAction func goback(_ sender: UIButton)  {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
