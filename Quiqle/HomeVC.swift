c758af06e9048290b3df6c70c398e9d1d791a7f8
Sikander Zeb <sikanderzb@gmail.com>
2018-05-31 02:31:52 +0400

    home screen and profile update screens added

---
+++ Quiqle/HomeVC.swift
@@ -0,0 +1,38 @@
+//
+//  HomeVC.swift
+//  Quiqle
+//
+//  Created by Sikander Zeb on 5/31/18.
+//  Copyright © 2018 Sikander Zeb. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    @IBOutlet weak var table: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+extension HomeVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        return cell!
+    }
+    
+}
